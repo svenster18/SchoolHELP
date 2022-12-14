38ad0b768961272808709382a2e747eeebe9580b
risakaniaputri <116688655+risakaniaputri@users.noreply.github.com>
2022-12-14 10:04:42 +0700

    add MainMenu

---
+++ src/schoolhelp/SchoolHELPGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package schoolhelp;
+
+/**
+ *
+ * @author risak
+ */
+public class SchoolHELPGUI {
+    public static void main(String[] args) {
+         MainMenu mainMenu = new MainMenu();
+         mainMenu.setVisible(true);
+    }
+}
